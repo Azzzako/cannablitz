96916731c1f9e22c686860d5a38c84bfbe243e85
Asael <zanduva@gmail.com>
2023-04-18 17:21:13 -0600

    creacion de formulario, el backend ya recibe la informacion

--- client/cannablitz_front/src/components/createProduct/createProduct.jsx
+++ client/cannablitz_front/src/components/createProduct/createProduct.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { saveNewProduct } from "../../redux/Actions/productActions";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { TextareaAutosize, Button } from "@mui/material";
+import './createProduct.css'
 
 export const CreateProduct = () => {
     const [newProduct, setNewProduct] = useState({});
     const dispatch = useDispatch();
     const config = {
         headers: { "Content-Type": "multipart/form-data" },
-      };
+    };
 
     const handleOnChange = (e) => {
+
         if (e.target.type === "file") {
             setNewProduct({
                 ...newProduct,
@@ -24,46 +29,66 @@ export const CreateProduct = () => {
     };
 
     const onSubmitProduct = (e) => {
-        e.preventDefault(); 
-        dispatch(saveNewProduct(newProduct, config));
-        setNewProduct({})
+        e.preventDefault();
+        if (newProduct.name && newProduct.price && newProduct.description && newProduct.image) {
+            dispatch(saveNewProduct(newProduct, config));
+            setNewProduct({
+                name: '',
+                price: '',
+                description: '',
+                image: ''
+            })
+        } else {
+            alert('Llena todos los campos')
+        }
+
     };
 
     console.log(newProduct);
 
     return (
         <React.Fragment>
-            <form onSubmit={onSubmitProduct}>
-                <label>Name: </label>{" "}
-                <input
-                    placeholder="Product Name"
+            <Box component='form'
+                noValidate onSubmit={onSubmitProduct} className="form_container">
+
+                <TextField
+                    color={newProduct?.name?.length > 0 ? 'success' : 'error'}
+                    id="outlined-basic"
+                    label='Product Name'
                     value={newProduct.name}
                     name="name"
                     onChange={handleOnChange}
                 />
-                <label>Price: </label>{" "}
-                <input
+
+                <TextField
+                    color={newProduct?.price?.length > 0 ? 'success' : 'error'}
+                    id="outlined-basic"
+                    label='Product Price'
                     type="number"
-                    placeholder="Product Price"
                     value={newProduct.price}
                     name="price"
                     onChange={handleOnChange}
                 />
-                <label>Description: </label>{" "}
-                <textarea
+
+                <TextareaAutosize
                     placeholder="Product Description"
                     value={newProduct.description}
                     name="description"
                     onChange={handleOnChange}
+                    
                 />
-                <label>Image: </label>{" "}
+
                 <input
                     type="file"
                     name="image"
                     onChange={handleOnChange}
                 />
-                <button type="submit">Enviar</button>
-            </form>
+                <Button
+                variant="contained" 
+                type="submit"
+                >Enviar
+                </Button>
+            </Box>
         </React.Fragment>
     );
 };
